f3da4b5dfc9b17a28a1b80831efd5871ac24b1bc
Silvan Strübi <silvan.struebi@mgb.ch>
2021-10-11 15:17:07 +0200

    sparx

--- wc-config.js
+++ wc-config.js
@@ -176,6 +176,10 @@
       url: 'web-components-cms-template/src/es/components/thirdParty/'
     },
     {
+      selector: 'sparx-a-',
+      url: 'web-components-cms-template-sparx/src/es/components/atoms/'
+    },
+    {
       selector: 'wc-a-',
       url: 'web-components/src/es/components/atoms/',
       separateFolder: true

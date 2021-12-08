0bd94575cd15de50da508c9162f3e6ae4723014a
Weedshaker <weedshaker@gmail.com>
2021-12-08 14:49:45 +0100

    search results h-tag styling

--- src/es/components/organisms/Body.js
+++ src/es/components/organisms/Body.js
@@ -55,6 +55,15 @@ export default class Body extends BaseBody {
         list-style: none;
         padding: 0;
       }
+      :host > main a > h1, :host > main a > h2, :host > main a > h3, :host > main a > h4, :host > main a > h5, :host > main a > h6 {
+        color: var(--a-color, var(--color-secondary, var(--color, pink)));
+        font-weight: var(--a-font-weight, var(--font-weight, normal));
+        text-align: var(--a-text-align, unset);
+        text-decoration: var(--a-text-decoration, var(--text-decoration, none));
+        text-underline-offset: var(--a-text-underline-offset, unset);
+        display: var(--a-display, inline);
+        margin: var(--a-margin, var(--content-spacing, unset)) auto;
+      }
       /* https://heroicons.com/ */
       :host > main a[href^="/"]:not(.no-icon):before {
         content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="white"><path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" /></svg>');

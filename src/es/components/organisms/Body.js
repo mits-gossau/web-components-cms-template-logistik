17082122413a7518ae37336543b33940b6edf54d
Weedshaker <weedshaker@gmail.com>
2021-12-08 15:12:59 +0100

    h-tag with link around styling

--- src/es/components/organisms/Body.js
+++ src/es/components/organisms/Body.js
@@ -57,12 +57,17 @@ export default class Body extends BaseBody {
       }
       :host > main a > h1, :host > main a > h2, :host > main a > h3, :host > main a > h4, :host > main a > h5, :host > main a > h6 {
         color: var(--a-color, var(--color-secondary, var(--color, pink)));
-        font-weight: var(--a-font-weight, var(--font-weight, normal));
-        text-align: var(--a-text-align, unset);
         text-decoration: var(--a-text-decoration, var(--text-decoration, none));
         text-underline-offset: var(--a-text-underline-offset, unset);
-        display: var(--a-display, inline);
-        margin: var(--a-margin, var(--content-spacing, unset)) auto;
+      }
+      :host > main a:hover > h1, :host > main a:active > h1, :host > main a:focus > h1,
+      :host > main a:hover > h2, :host > main a:active > h2, :host > main a:focus > h2,
+      :host > main a:hover > h3, :host > main a:active > h3, :host > main a:focus > h3,
+      :host > main a:hover > h4, :host > main a:active > h4, :host > main a:focus > h4,
+      :host > main a:hover > h5, :host > main a:active > h5, :host > main a:focus > h5,
+      :host > main a:hover > h6, :host > main a:active > h6, :host > main a:focus > h6 {
+        color: var(--a-color-hover, var(--color-hover-secondary, var(--color-hover, var(--color, green))));
+        text-decoration: var(--a-text-decoration-hover, var(--text-decoration-hover, var(--a-text-decoration, var(--text-decoration, none))));
       }
       /* https://heroicons.com/ */
       :host > main a[href^="/"]:not(.no-icon):before {
@@ -71,13 +76,13 @@ export default class Body extends BaseBody {
       :host > main a[href^="http"]:not(.no-icon):before {
         content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="white"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" /><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" /></svg>');
       }
-      :host > main a[href$=".pdf"]:before {
+      :host > main a[href$=".pdf"]:not(.no-icon):before {
         content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="white"><path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" /></svg>');
       }
-      :host > main a[href$=".xls"]:before {
+      :host > main a[href$=".xls"]:not(.no-icon):before {
         content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="white"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd" /></svg>');
       }
-      :host > main a[href^="/"]:not(.no-icon):before, :host > main a[href^="http"]:not(.no-icon):before, :host > main a[href$=".pdf"]:before, :host > main a[href$=".xls"]:before {
+      :host > main a[href^="/"]:not(.no-icon):before, :host > main a[href^="http"]:not(.no-icon):before, :host > main a[href$=".pdf"]:not(.no-icon):before, :host > main a[href$=".xls"]:not(.no-icon):before {
         background-color: var(--a-color);
         border-radius: 0.25rem;
         display: inline-block;
@@ -87,7 +92,7 @@ export default class Body extends BaseBody {
         vertical-align: middle;
         width: 1.25rem;
       }
-      :host > main a[href^="/"]:not(.no-icon):hover:before, :host > main a[href^="http"]:not(.no-icon):hover:before, :host > main a[href$=".pdf"]:hover:before, :host > main a[href$=".xls"]:hover:before {
+      :host > main a[href^="/"]:not(.no-icon):hover:before, :host > main a[href^="http"]:not(.no-icon):hover:before, :host > main a[href$=".pdf"]:not(.no-icon):hover:before, :host > main a[href$=".xls"]:not(.no-icon):hover:before {
         background-color: var(--a-color-hover);
       }
       :host > main img {

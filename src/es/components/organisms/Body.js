caaa070a5e98d71f9b99feb85b9e2e9ce6c9f5d8
Weedshaker <weedshaker@gmail.com>
2022-01-20 12:14:35 +0100

    content max width added

--- src/es/components/organisms/Body.js
+++ src/es/components/organisms/Body.js
@@ -23,6 +23,12 @@ export default class Body extends BaseBody {
   renderCSS () {
     super.renderCSS()
     this.css = /* css */`
+      :host(.content-max-width) > main > * {
+        max-width: var(--content-max-width, none);
+      }
+      :host(.content-max-width-not-web-component) > main > span, :host(.content-max-width-not-web-component) > main > div, :host(.content-max-width-not-web-component) > main > p, :host(.content-max-width-not-web-component) > main > a, :host(.content-max-width-not-web-component) > main > article, :host(.content-max-width-not-web-component) > main > ul, :host(.content-max-width-not-web-component) > main > ol, :host(.content-max-width-not-web-component) > main > section, :host(.content-max-width-not-web-component) > main > h1, :host(.content-max-width-not-web-component) > main > h2, :host(.content-max-width-not-web-component) > main > h3, :host(.content-max-width-not-web-component) > main > h4, :host(.content-max-width-not-web-component) > main > h5, :host(.content-max-width-not-web-component) > main > h6 {
+        max-width: var(--content-max-width-not-web-component, none);
+      }
       :host > main > *:not(style) {
         display: block;
       }
